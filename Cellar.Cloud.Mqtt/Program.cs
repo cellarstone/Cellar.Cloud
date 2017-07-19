f0d2d667cd92f58ed19d2cd09ece27efbcc5c3d8
Kellerstein Lukas <lukas.kellerstein@cellarstone.eu>
2017-07-19 19:54:13 +0200

    Add project files.

---
+++ Cellar.Cloud.Mqtt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cellar.Cloud.Mqtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
